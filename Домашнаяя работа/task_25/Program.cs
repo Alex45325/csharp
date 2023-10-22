b67f6fa61cc6699f4c0b064c44fcec783591327c
Alex <naruto.xd1@mail.ru>
2023-10-22 16:57:38 +0300

    Добавил задание

---
+++ Домашнаяя работа/task_25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Получаем числа A и B от пользователя
+        Console.Write("Введите число A: ");
+        int A = int.Parse(Console.ReadLine());
+        Console.Write("Введите число B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        // Инициализируем переменную для хранения результата
+        int результат = 1;
+
+        // Цикл для возвведения числа A в степень B
+        for (int i = 1; i <= B; i++)
+        {
+            результат *= A;
+        }
+
+        // Выводим результат
+        Console.WriteLine($"Результат: {результат}");
+    }
+}
---
+++ Домашнаяя работа/task_29/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем массив из 8 элементов
+        int[] массив1 = {1, 2, 5, 7, 19};
+        int[] массив2 = {6, 1, 33};
+
+        // Выводим массивы на экран
+        Console.Write("[");
+        ВывестиМассив(массив1);
+        Console.WriteLine("]");
+
+        Console.Write("[");
+        ВывестиМассив(массив2);
+        Console.WriteLine("]");
+    }
+
+    // Функция для вывода массива на экран
+    static void ВывестиМассив(int[] массив)
+    {
+        for (int i = 0; i < массив.Length; i++)
+        {
+            Console.Write(массив[i]);
+            // Если элемент не последний, выводим запятую и пробел
+            if (i < массив.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
