cd6df82933dc2fd13b17939911858412a9066507
Alex <naruto.xd1@mail.ru>
2023-11-03 11:37:06 +0200

    Добавил задание

---
+++ Домашнаяя работа/Task_41/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрашиваем у пользователя количество чисел, которые он хочет ввести
+        Console.Write("Введите количество чисел: ");
+        int M = int.Parse(Console.ReadLine());
+
+        // Инициализируем счетчик положительных чисел
+        int countPositive = 0;
+
+        // Считываем числа и проверяем, являются ли они положительными
+        for (int i = 0; i < M; i++)
+        {
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0)
+            {
+                countPositive++;
+            }
+        }
+
+        // Выводим количество положительных чисел
+        Console.WriteLine("Количество положительных чисел: " + countPositive);
+    }
+}
---
+++ Домашнаяя работа/task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод значений b1, k1, b2 и k2 пользователем
+        Console.Write("Введите значение b1: ");
+        double b1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k1: ");
+        double k1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b2: ");
+        double b2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k2: ");
+        double k2 = double.Parse(Console.ReadLine());
+
+        // Расчет точки пересечения прямых
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        // Вывод результатов
+        Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+    }
+}
