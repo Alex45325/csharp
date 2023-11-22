1bbf08a6d662cf248df4a87648cb5bc32adff108
Alex <naruto.xd1@mail.ru>
2023-11-22 21:41:43 +0200

    Добавил задание

---
+++ Домашнаяя работа/task64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Результат: " + GetNaturalNumbers(N));
+    }
+
+    static string GetNaturalNumbers(int n)
+    {
+        if (n == 1)
+        {
+            return "1";
+        }
+        else
+        {
+            return n + ", " + GetNaturalNumbers(n - 1);
+        }
+    }
+}
---
+++ Домашнаяя работа/task66/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int sum = GetSumOfNaturalNumbers(M, N);
+
+        Console.WriteLine($"Сумма натуральных элементов от {M} до {N}: {sum}");
+    }
+
+    static int GetSumOfNaturalNumbers(int m, int n)
+    {
+        if (m > n)
+        {
+            throw new ArgumentException("Значение M должно быть меньше или равно N.");
+        }
+
+        if (m == n)
+        {
+            return m;
+        }
+        else
+        {
+            return m + GetSumOfNaturalNumbers(m + 1, n);
+        }
+    }
+}
